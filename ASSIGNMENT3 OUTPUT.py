b0325642a09e1ddda80ae08c6c08654604492ec8
jatinder pal singh <97720545+Jatinderbit@users.noreply.github.com>
2022-02-14 23:35:58 +0530

    21104059_JATINDER PAL SINGH

---
+++ ASSIGNMENT3 OUTPUT.py
@@ -0,0 +1,65 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: C:\Users\Mjolnir\Desktop\PYTHON\ASSIGNMENT 3.PY ===========
+QUESTION 1:
+
+ENTER ANY STRING: physics
+{'p': 1, 'h': 1, 'y': 1, 's': 2, 'i': 1, 'c': 1}
+QUESTION 2:
+
+Input a year: 2003
+Input a month [1-12]: 12
+Input a day [1-31]: 31
+The next date is [yyyy-mm-dd] 2004-1-1.
+QUESTION 3:
+
+Enter elements of a list separated by space 3 6 9
+list:  3 6 9
+[(3, 9), (6, 36), (9, 81)]
+QUESTION 4:
+
+Enter your marks: 9
+Your Grade is 'A' and Excellent Performance
+QUESTION 5:
+
+ABCDEFGHI
+ ABCDEFG
+  ABCDE
+   ABC
+    A
+QUESTION 6:
+
+Enter SID: 21104059
+Enter Name: Jatinder pal singh
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 2113454
+Enter Name: Salman
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 2109342
+Enter Name: Ustad Rashid Khan
+Do you want to enter another student entry(Y or N):N
+<a> {21104059: 'Jatinder pal singh', 2113454: 'Salman', 2109342: 'Ustad Rashid Khan'}
+<b> {21104059: 'Jatinder pal singh', 2113454: 'Salman', 2109342: 'Ustad Rashid Khan'}
+<c> {2109342: 'Ustad Rashid Khan', 2113454: 'Salman', 21104059: 'Jatinder pal singh'}
+Search Name with SID: 21104059
+<d> Jatinder pal singh
+QUESTION 7:
+
+ENTER THE NUMBER OF TERMS IN THE SERIES: 6
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+AVERAGE : 2.0
+QUESTION 8:
+
+<a> {1, 3, 5, 6, 8}
+<b> {3, 6, 8, 9, 13, 17}
+<c> {1, 2, 4, 5}
+<d> {6, 7, 8, 9, 10}
+<e> {10, 7}
+
